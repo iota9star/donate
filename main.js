f06e87d60842b98c54bf7c698189f03c8c06b30f
iota9star <iota9star@foxmail.com>
2023-05-21 18:18:35 +0800

    🔥 More info

--- main.js
+++ main.js
@@ -11,15 +11,14 @@ const pays = [
     {
         label: "WechatPay",
         src: "wechat.png",
-        url: "wxp://f2f0JfXzfvXcA1PIqV0-jN0b7nd3W1Rlr1kzwvIdJjDzo38",
         id: "wechat"
     },
 ];
 const qrs = pays.map((e) => {
-    return `<a href="${e.url}" id="${e.id}"><img src="${e.src}" alt="${e.label}" /><br><button>${e.label}</button></a>`;
+    return `<a href="${e.url || '#' + e.id}" id="${e.id}"><img src="${e.src}" alt="${e.label}" /><br><button ${e.url ? '' : 'disabled'}>${e.label}</button><br><label>${e.url ? '&nbsp;' : '仅支持微信直接扫码或者下载图片后在微信内识别图片扫码'}</label></a>`;
 }).join("");
 
-document.querySelector('#app').innerHTML = `<div>${qrs}<h1>Thank you, master!</h1><p>You're one in a million. </p></div>`
+document.querySelector('#app').innerHTML = `<div><div class="qrs">${qrs}</div><h1>Thank you, master!</h1><p>You're one in a million. </p></div>`
 
 const params = new URLSearchParams(window.location.search);
 if (params.has('p')) {
